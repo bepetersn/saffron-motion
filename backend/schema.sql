885a3456498ac6443db612f70cfe36847165febd
Brian Everett Peterson <bepetersn@gmail.com>
2021-09-03 20:29:38 +0000

    Add data model

--- backend/schema.sql
+++ backend/schema.sql
@@ -2,19 +2,44 @@
 -- Drop any existing data and create empty tables.
 
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS status;
+DROP TABLE IF EXISTS status_changes;
+DROP TABLE IF EXISTS pomodoro;
+DROP TABLE IF EXISTS pomodoro_changes;
 
 CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
 
-CREATE TABLE post (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
+CREATE TABLE statuses (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id);
+  status_name VARCHAR(510),
+  time_last_updated TIMESTAMP
+);
+
+CREATE TABLE pomodoro (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id),
+  time_started TIMESTAMP,
+  session_type VARCHAR,
+  running BOOLEAN,
+  clean BOOLEAN,
+  time_last_paused TIMESTAMP
+);
+
+CREATE TABLE status_changes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id),
+  time TIMESTAMP,
+  state JSON
+);
+
+CREATE TABLE pomodoro_changes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id),
+  time TIMESTAMP,
+  state JSON
 );
