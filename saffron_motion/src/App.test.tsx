2a5cec7090f8f3f2bd034ee969ac48bd48233c9d
Brian Everett Peterson <bepetersn@gmail.com>
2021-09-06 22:30:01 +0000

    Tests and basic layout

--- saffron_motion/src/App.test.tsx
+++ saffron_motion/src/App.test.tsx
@@ -2,8 +2,52 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders mode toggles', () => {
   render(<App />);
-  const linkElement: HTMLElement = screen.getByText(/hi there/i);
-  expect(linkElement).toBeInTheDocument();
+  let buttonElement: HTMLElement = screen.getByText(/short break/i);
+  expect(buttonElement).toBeInTheDocument();
+
+  buttonElement = screen.getByText(/long break/i);
+  expect(buttonElement).toBeInTheDocument();
+
+  buttonElement = screen.getByText(/pomodoro/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders start time (25 minutes)', () => {
+  render(<App />);
+  const textElement: HTMLElement = screen.getByText(/25:00/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders action buttons', () => {
+  render(<App />);
+  let buttonElement: HTMLElement = screen.getByText(/start/i);
+  expect(buttonElement).toBeInTheDocument();
+  buttonElement = screen.getByText(/stop/i);
+  expect(buttonElement).toBeInTheDocument();
+  buttonElement = screen.getByText(/reset/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders action buttons', () => {
+  render(<App />);
+  let buttonElement: HTMLElement = screen.getByText(/start/i);
+  expect(buttonElement).toBeInTheDocument();
+  buttonElement = screen.getByText(/stop/i);
+  expect(buttonElement).toBeInTheDocument();
+  buttonElement = screen.getByText(/reset/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders status', () => {
+  render(<App />);
+  const statusElement: HTMLElement = screen.getByText(/status/i);
+  expect(statusElement).toBeInTheDocument();
+});
+
+test('renders status toggle button', () => {
+  render(<App />);
+  const buttonElement: HTMLElement = screen.getByText(/toggle to planning/i);
+  expect(buttonElement).toBeInTheDocument();
 });
--- saffron_motion/src/App.tsx
+++ saffron_motion/src/App.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import {
+  CSSReset, Flex, ChakraProvider, ButtonGroup, Button, Stack, Center, Text, Box, Tag,
+} from '@chakra-ui/react';
 import './App.css';
 
 function App() {
+  const timeInMinutes = 25;
   return (
-    <div>
-      <p>Hi there</p>
-    </div>
+    <ChakraProvider>
+      <Flex direction="column" align="center" justify="center">
+        <CSSReset />
+        <Stack>
+          <Flex justify="center" align="center" w="100%" h="20vh">
+            <ButtonGroup>
+              <Button>Pomodoro</Button>
+              <Button>Short Break</Button>
+              <Button>Long Break</Button>
+            </ButtonGroup>
+          </Flex>
+          <Flex justify="center" align="center" w="100%" h="30vh">
+            <Center>
+              <Text fontSize="8vw">
+                {timeInMinutes}
+                :00
+              </Text>
+            </Center>
+          </Flex>
+          <Flex justify="center" align="center" w="100%" h="20vh">
+            <ButtonGroup>
+              <Button>Start</Button>
+              <Button>Stop</Button>
+              <Button>Reset</Button>
+            </ButtonGroup>
+          </Flex>
+          <Flex justify="center" align="center" w="100%" h="20vh">
+            <Box>
+              <Flex alignItems="center">
+                <Text>
+                  Status:
+                  {' '}
+                </Text>
+                <Tag>
+                  <Text fontSize="3vw">Working</Text>
+                </Tag>
+                <Button bgColor="lightSkyBlue">
+                  Toggle to Planning
+                </Button>
+              </Flex>
+            </Box>
+          </Flex>
+        </Stack>
+      </Flex>
+    </ChakraProvider>
   );
 }
 
