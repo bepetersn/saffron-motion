f839af47a446cc3eba49acab8031271c4b4f368b
Brian Everett Peterson <bepetersn@gmail.com>
2021-09-09 08:26:00 +0000

    Actually add Time file and tests

---
+++ saffron_motion/src/Time.test.ts
@@ -0,0 +1,48 @@
+import { calculateTimeRemaining, parseDateDiff } from './Time';
+
+test('calc test -- countdown 7sec', () => {
+  const timeStarted = Date.now() - 7000;
+  const lastRecordedElapsed = 7000;
+  const lastRecordedTime = Date.now();
+  expect(calculateTimeRemaining(
+    timeStarted, lastRecordedTime, lastRecordedElapsed,
+  )).toEqual(parseDateDiff('24:53'));
+});
+
+test('calc test -- countdown with pause/start', () => {
+  // Started 11 seconds ago
+  const timeStarted = Date.now() - 11000;
+  // Paused 8 seconds ago -- Not visible -- (LRT = Date.now() - 8000)
+  // 3 seconds had passed
+  const lastRecordedElapsed = 3000;
+  // Started again 4 seconds ago
+  const lastRecordedTime = Date.now() - 4000;
+  // 4 seconds have passed
+
+  // 3sec + 4sec = 7sec passed
+  expect(calculateTimeRemaining(
+    timeStarted, lastRecordedTime, lastRecordedElapsed,
+  )).toEqual(parseDateDiff('24:53'));
+});
+
+test('calc test -- countdown with multiple pause/start', () => {
+  // Started 15 seconds ago
+  const timeStarted = Date.now() - 15000;
+  // Paused 10 seconds ago -- Not visible -- (LRT = Date.now() - 8000)
+  // 5 seconds had passed -- Not visible
+  let lastRecordedElapsed = 5000;
+  // Resumed 7 seconds ago
+  // NOT VISIBLE -- let lastRecordedTime = Date.now() - 7000;
+  // Paused again 5 seconds ago
+  // NOT VISIBLE -- lastRecordedTime = Date.now() - 5000;
+  // 2 seconds had passed, additional
+  lastRecordedElapsed += 2000;
+  // Resumed 3 seconds ago
+  const lastRecordedTime = Date.now() - 3000;
+  // 3 seconds have passed
+
+  // 5sec + 2sec + 3sec = 10sec passed
+  expect(calculateTimeRemaining(
+    timeStarted, lastRecordedTime, lastRecordedElapsed,
+  )).toEqual(parseDateDiff('24:50'));
+});
---
+++ saffron_motion/src/Time.ts
@@ -0,0 +1,33 @@
+// 25 minutes in milliseconds
+const INITIAL_TIME_IN_MILLIS = 25 * 60 * 1000;
+
+export function parseDateDiff(dateDiffStr: string) {
+  /* convert from mm:ss to diff */
+  const [minutesStr, secondsStr] = dateDiffStr.split(':');
+  const [minutes, seconds] = [parseInt(minutesStr, 10), parseInt(secondsStr, 10)];
+  return (minutes * 60 * 1000) + (seconds * 1000);
+}
+
+export function formatDateDiff(dateDiff: number) {
+  /* convert diff to mm:ss format */
+  const minutes = Math.floor(dateDiff / (1000 * 60));
+  const seconds = Math.floor(
+    (dateDiff / 1000) - (minutes * 60) // eslint-disable-line comma-dangle
+  );
+  const minutesStr = String(minutes).padStart(2, '0');
+  const secondsStr = String(seconds).padStart(2, '0');
+  return `${minutesStr}:${secondsStr}`;
+}
+
+export function calculateTimeRemaining(
+  timeStarted: number, lastRecordedTime: number, lastRecordedElapsed: number,
+) {
+  const lastRecorded = lastRecordedTime || timeStarted;
+  const newElapsed = Date.now() - lastRecorded;
+  const millisRemaining = (
+    INITIAL_TIME_IN_MILLIS
+    - lastRecordedElapsed
+    - newElapsed
+  );
+  return millisRemaining;
+}
