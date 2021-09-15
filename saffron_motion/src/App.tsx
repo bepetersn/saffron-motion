2787c24523be765b0b9f5e3a599b45b6455a75fe
Brian Everett Peterson <bepetersn@gmail.com>
2021-09-15 18:06:45 +0000

    Center content & status container better

--- saffron_motion/src/App.tsx
+++ saffron_motion/src/App.tsx
@@ -68,56 +68,60 @@ export default function App(): ReactElement {
 
   return (
     <ChakraProvider>
-      <Flex direction="row" justifyContent="space-around">
-        <CSSReset />
-        <Flex id="timerContainer" flexDirection="column" justifyContent="stretch">
-          <Flex id="modeButtons" w="100%" h="20vh" alignItems="center">
-            <ButtonGroup>
-              <Button>Pomodoro</Button>
-              <Button>Short Break</Button>
-              <Button>Long Break</Button>
-            </ButtonGroup>
-          </Flex>
-          <Flex id="timer" w="100%" h="30vh" justify="center" align="center">
-            <Text fontSize="8vw">
-              {getFormattedTimeRemaining(timerState)}
-            </Text>
-          </Flex>
-          <Flex id="timerButtons" justify="center" align="center" w="100%" h="20vh">
-            <ButtonGroup>
-              <Button onClick={() => dispatch({ type: ActionType.START })}>Start</Button>
-              <Button onClick={() => dispatch({ type: ActionType.PAUSE })}>Pause</Button>
-              <Button onClick={() => dispatch({ type: ActionType.RESET })}>Reset</Button>
-            </ButtonGroup>
-          </Flex>
-        </Flex>
-        <Flex id="statusContainer" flex-direction="column" flexWrap="wrap" justify="center" align="center" h="20vh" w="30vw">
-          <Flex flexDirection="row" alignItems="center">
-            <Text fontSize="3vw">
-              Status:
-              {' '}
-            </Text>
-            <Tag bgColor="lightSkyBlue" ml="5px" mr="10px" border="1px solid black">
-              <Text fontSize="3vw">
-                {workingStatus}
+      <Flex>
+        <Flex direction="row" justifyContent="space-around" m="5vw">
+          <CSSReset />
+          <Flex id="timerContainer" flexDirection="column" justifyContent="stretch">
+            <Flex id="modeButtons" w="100%" h="20vh" alignItems="center">
+              <ButtonGroup>
+                <Button>Pomodoro</Button>
+                <Button>Short Break</Button>
+                <Button>Long Break</Button>
+              </ButtonGroup>
+            </Flex>
+            <Flex id="timer" w="100%" h="30vh" justify="center" align="center">
+              <Text fontSize="8vw">
+                {getFormattedTimeRemaining(timerState)}
               </Text>
-            </Tag>
+            </Flex>
+            <Flex id="timerButtons" justify="center" align="center" w="100%" h="20vh">
+              <ButtonGroup>
+                <Button onClick={() => dispatch({ type: ActionType.START })}>Start</Button>
+                <Button onClick={() => dispatch({ type: ActionType.PAUSE })}>Pause</Button>
+                <Button onClick={() => dispatch({ type: ActionType.RESET })}>Reset</Button>
+              </ButtonGroup>
+            </Flex>
           </Flex>
-          {running && (
+          <Flex id="statusContainer" direction="column" wrap="wrap" justify="center" align="center" w="30vw" grow={1}>
             <Flex>
-              <Button
-                bgColor="goldenrod"
-                onClick={() => dispatch({ type: ActionType.TOGGLE_WORKING_STATUS })}
-              >
-                Toggle
-              </Button>
+              <Flex direction="row" align="center">
+                <Text fontSize="3vw">
+                  Status:
+                  {' '}
+                </Text>
+                <Tag bgColor="lightSkyBlue" ml="5px" mr="10px" border="1px solid black">
+                  <Text fontSize="3vw">
+                    {workingStatus}
+                  </Text>
+                </Tag>
+              </Flex>
+              {running && (
+                <Flex alignSelf="center">
+                  <Button
+                    bgColor="goldenrod"
+                    onClick={() => dispatch({ type: ActionType.TOGGLE_WORKING_STATUS })}
+                  >
+                    Toggle
+                  </Button>
+                </Flex>
+              )}
             </Flex>
-          )}
-          {running && (
-            <Flex justify="center">
-              <Input placeholder="What are you working on...?" />
-            </Flex>
-          )}
+            {running && (
+              <Flex justify="center">
+                <Input placeholder="What are you working on...?" />
+              </Flex>
+            )}
+          </Flex>
         </Flex>
       </Flex>
     </ChakraProvider>
